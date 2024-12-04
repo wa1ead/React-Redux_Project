57c08c175085ba21489e1b1ca597d4c21c387f60
wa1ead <waliddraa2@gmail.com>
2024-12-04 13:23:09 +0100

    creating aside component

---
+++ src/components/Aside.jsx
@@ -0,0 +1,33 @@
+export default function Aside() {
+  return (
+    <section className="w-[20%] float-start bg-gray-200">
+      <div className="flex flex-col gap-4">
+        <div>
+          <input type="text" id="search" />
+        </div>
+        <div className="flex flex-col gap-4">
+          <div>
+            <h2 className='text-xl font-semibold'>TEAMS</h2>
+            <ul className='text-md font-light'>
+              <li>Raja CA</li>
+              <li>Liverpool FC</li>
+              <li>Real Madrid</li>
+              <li>AC Milan</li>
+              <li>Borussia Dortmund</li>
+            </ul>
+          </div>
+          <div>
+            <h2>NATIONAL TEAMS</h2>
+            <ul>
+              <li>Italy</li>
+              <li>England</li>
+              <li>Germany</li>
+              <li>France</li>
+              <li>Spain</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
