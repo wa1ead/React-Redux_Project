397cb9f39387124f5e90c318de2f89f689509cdd
wa1ead <waliddraa2@gmail.com>
2024-12-04 17:31:00 +0100

    adding border bottom to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="text-gray-600 body-font">
+    <header className="text-gray-600 body-font border-2 border-b-gray-900">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <svg
