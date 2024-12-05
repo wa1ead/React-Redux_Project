a39a0f896c0a86c38a013023b6cb941fbb30fbb2
wa1ead <waliddraa2@gmail.com>
2024-12-05 11:32:37 +0100

    creating the main component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Aside from "./components/Aside";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Carousel from "./components/Carousel";
+import Main from "./components/Main";
 export default function App() {
   return (
     <>
       <Header />
       <Carousel />
+      <Main/>
       <Footer />
     </>
   );
