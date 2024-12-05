2ca79e7c7eb9da5340932b8077e9da648f6dbd6b
wa1ead <waliddraa2@gmail.com>
2024-12-05 12:08:41 +0100

    creating the best sellers component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Carousel from "./components/Carousel";
 import Main from "./components/Main";
+import BestSellers from "./components/BestSellers";
 export default function App() {
   return (
     <>
       <Header />
       <Carousel />
-      <Main/>
+      <Main />
+      <BestSellers />
       <Footer />
     </>
   );
---
+++ src/components/BestSellers.jsx
@@ -0,0 +1,157 @@
+export default function BestSellers() {
+  return (
+    <section className="container mx-auto">
+      <div>
+        <h1 className="text-gray-950 font-semibold text-2xl">Best Sellers</h1>
+      </div>
+      <div class="container px-5 py-12 mx-auto">
+        <div class="flex flex-wrap -m-4">
+          <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <a class="block relative h-48 rounded overflow-hidden">
+              <img
+                alt="ecommerce"
+                class="object-fill object-center w-full h-full block"
+                src="https://www.le7sorelle.it/94228-large_default/2006-Home-Italy-shirt-Puma-Totti-10.jpg"
+              />
+            </a>
+            <div class="mt-4">
+              <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
+                PUMA
+              </h3>
+              <h2 class="text-gray-900 title-font text-lg font-medium">
+                ITALY 2006 World Cup
+              </h2>
+              <p class="mt-1">$79.99</p>
+            </div>
+          </div>
+          <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <a class="block relative h-48 rounded overflow-hidden">
+              <img
+                alt="ecommerce"
+                class="object-fill object-center w-full h-full block"
+                src="https://maniacstore.in/cdn/shop/files/Screenshot-2024-06-03-00-08-09-59_40deb401b9ffe8e1df2f1cc5ba480b12_2.jpg?v=1717353661"
+              />
+            </a>
+            <div class="mt-4">
+              <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
+                NIKE
+              </h3>
+              <h2 class="text-gray-900 title-font text-lg font-medium">
+                ARSENAL 03/04 SHIRT
+              </h2>
+              <p class="mt-1">$59.99</p>
+            </div>
+          </div>
+          <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <a class="block relative h-48 rounded overflow-hidden">
+              <img
+                alt="ecommerce"
+                class="object-fill object-center w-full h-full block"
+                src="https://www.vintagefootballshirts.com/cdn/shop/files/2008-09-celtic-nike-home-shirt-64607-1.jpg?v=1730189760&width=800"
+              />
+            </a>
+            <div class="mt-4">
+              <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
+                NIKE
+              </h3>
+              <h2 class="text-gray-900 title-font text-lg font-medium">
+                CELTIC 08/09 SHIRT
+              </h2>
+              <p class="mt-1">$29.99</p>
+            </div>
+          </div>
+          <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <a class="block relative h-48 rounded overflow-hidden">
+              <img
+                alt="ecommerce"
+                class="object-fill object-center w-full h-full block"
+                src="https://www.retrofootball.co.uk/media/catalog/product/cache/8/small_image/280x/9df78eab33525d08d6e5fb8d27136e95/r/e/real_madrid_2011_2012_v13659_retrofootball.jpg"
+              />
+            </a>
+            <div class="mt-4">
+              <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
+                ADIDAS
+              </h3>
+              <h2 class="text-gray-900 title-font text-lg font-medium">
+                REAL MADRID 11/12 SHIRT
+              </h2>
+              <p class="mt-1">$99.99</p>
+            </div>
+          </div>
+          <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <a class="block relative h-48 rounded overflow-hidden">
+              <img
+                alt="ecommerce"
+                class="object-fill object-center w-full h-full block"
+                src="https://premierretros.co.uk/wp-content/uploads/2022/03/0206065C-4525-44C9-9C20-CA94C12D14A0-scaled.jpeg"
+              />
+            </a>
+            <div class="mt-4">
+              <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
+                ADIDAS
+              </h3>
+              <h2 class="text-gray-900 title-font text-lg font-medium">
+                AC MILAN 06/07 SHIRT
+              </h2>
+              <p class="mt-1">$89.99</p>
+            </div>
+          </div>
+          <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <a class="block relative h-48 rounded overflow-hidden">
+              <img
+                alt="ecommerce"
+                class="object-fill object-center w-full h-full block"
+                src="https://footballshirtcollective.com/cdn/shop/files/2008-09-barcelona-vintage-nike-home-football-shirt-xxl-42477592609015_5000x.jpg?v=1713967099"
+              />
+            </a>
+            <div class="mt-4">
+              <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
+                NIKE
+              </h3>
+              <h2 class="text-gray-900 title-font text-lg font-medium">
+                FC BARCELONA 08/09 SHIRT
+              </h2>
+              <p class="mt-1">$09.99</p>
+            </div>
+          </div>
+          <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <a class="block relative h-48 rounded overflow-hidden">
+              <img
+                alt="ecommerce"
+                class="object-fill object-center w-full h-full block"
+                src="https://i0.wp.com/classicscollectivestore.com/wp-content/uploads/2021/11/9899MANUHOME.png?fit=800%2C800&ssl=1"
+              />
+            </a>
+            <div class="mt-4">
+              <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
+                UMBRO
+              </h3>
+              <h2 class="text-gray-900 title-font text-lg font-medium">
+                MAN UNITED 98/99 SHIRT
+              </h2>
+              <p class="mt-1">$59.99</p>
+            </div>
+          </div>
+          <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <a class="block relative h-48 rounded overflow-hidden">
+              <img
+                alt="ecommerce"
+                class="object-cover object-center w-full h-full block"
+                src="https://classicfootballshirts.co.uk/pub/media/catalog/product/t/3/t35266490-1_mibc3mssb5dui1cm.jpg"
+              />
+            </a>
+            <div class="mt-4">
+              <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
+                KAPPA
+              </h3>
+              <h2 class="text-gray-900 title-font text-lg font-medium">
+                RAJA CA 03/04 SHIRT
+              </h2>
+              <p class="mt-1">$89.99</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
