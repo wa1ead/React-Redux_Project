7d1091730380d85ea4587af792750f69941e23e8
wa1ead <waliddraa2@gmail.com>
2024-12-09 15:16:49 +0100

    installing react-redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 import { store } from "./app/store";
 import "./index.css";
 import App from "./App.jsx";
