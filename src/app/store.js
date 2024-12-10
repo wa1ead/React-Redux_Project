de95afbf79372affe52691394de45c27fb5fb9f6
wa1ead <waliddraa2@gmail.com>
2024-12-10 17:47:48 +0100

    adding products slice into the store reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import sliderReducer from "../features/slider/sliderSlice";
+import productsSlice from "../features/products/productsSlice";
 
 export const store = configureStore({
   reducer: {
     slider: sliderReducer,
+    products: productsSlice,
   },
 });
