f196a557bd0369e546c08f98cfd75813cada08bf
wa1ead <waliddraa2@gmail.com>
2024-12-11 11:37:20 +0100

    adding shopping slice into the store reducer

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import sliderReducer from "../features/slider/sliderSlice";
 import productsSlice from "../features/products/productsSlice";
 import leaguesSlice from "../features/leagues/leaguesSlice";
+import ShoppingSlice from "../features/shopping/ShoppingSlice";
 
 export const store = configureStore({
   reducer: {
     slider: sliderReducer,
     products: productsSlice,
     leagues: leaguesSlice,
+    shopping: ShoppingSlice,
   },
 });
