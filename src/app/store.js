80cea56cc4c8f7e00ef13445f2667d6a5635ea1d
wa1ead <waliddraa2@gmail.com>
2024-12-11 10:36:31 +0100

    adding leagues slice into the store reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import sliderReducer from "../features/slider/sliderSlice";
 import productsSlice from "../features/products/productsSlice";
+import leaguesSlice from "../features/leagues/leaguesSlice";
 
 export const store = configureStore({
   reducer: {
     slider: sliderReducer,
     products: productsSlice,
+    leagues: leaguesSlice,
   },
 });
